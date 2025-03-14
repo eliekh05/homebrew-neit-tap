91f0d8477da89016119f54cb50d1a4a9f333e895
Elie Khalil <eliekh05@gmail.com>
2025-03-14 13:34:25 +0200

    Update neit.rb

--- neit.rb
+++ neit.rb
@@ -2,7 +2,7 @@ class Neit < Formula
   desc "NEIT Mac builds for easy installation"
   homepage "https://github.com/eliekh05/neit-mac-builds"
   url "https://github.com/eliekh05/neit-mac-builds/releases/download/0.1.0/neit.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "2fe11a7e27bef5cbb5ae1774a0869c0e5a6a86450d51780bfa53299337588af2"
   license "MIT"
 
   def install
